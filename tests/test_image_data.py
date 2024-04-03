c114f6f287f9e77e11b57f01d2231ae5e4f27d59
stalgo <joakim.polden@gmail.com>
2024-04-03 14:18:48 +0200

    added test for imagedata

--- tests/test_image_data.py
+++ tests/test_image_data.py
@@ -1,4 +1,6 @@
 import unittest
+
+from tomlkit import value
 from report.image_data import ImageData
 
 
@@ -6,9 +8,15 @@ class TestImageData(unittest.TestCase):
     def setUp(self):
         self.image = ImageData()
 
-    def test_should_return_string(self):
+    def test_should_raise_type_error_for_non_string(self):
         with self.assertRaises(TypeError):
             self.image.image_location = 3
+            self.image.about_image = 2
+            self.image.header_image = 2
+
+    def test_should_raise_value_error_when_empty_string(self):
+        with self.assertRaises(ValueError):
+            self.image.image_location = ""
 
 
 if __name__ == "__main__":
