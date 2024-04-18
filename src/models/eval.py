1ce4d477c44149b6d2000112c9548fcd18d1b517
stalgo <joakim.polden@gmail.com>
2024-04-18 22:53:39 +0200

    remove mistype

--- src/models/eval.py
+++ src/models/eval.py
@@ -27,7 +27,7 @@ class Evaluator:
         Initializes the Evaluator object with command-line arguments and loads the trained model.
         :param args: Command-line arguments specifying evaluation parameters.
         """
-        self.args = args|
+        self.args = args
         self.dataset = dataset
         self._weightmanager = WeightManager()
         self._weightmanager.current_model = model_builder
