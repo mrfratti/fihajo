36cd41bb3d5577b943acb761980d02ccf11d8daa
Harykaran Lambotharan <l.harykaran@gmail.com>
2024-05-06 12:19:05 +0200

    updated

--- src/report_interactive/interactive_html_generator.py
+++ src/report_interactive/interactive_html_generator.py
@@ -2,13 +2,11 @@ import os
 import json
 
 from src.report_interactive.interactive_html_data import Interactive_Html_Data
-# from src.report_interactive
+
 
 class Interactive_Html_Generator:
     def __init__(self):
         self._html_data = Interactive_Html_Data()
-
-
 
     def generate(self):
 
@@ -20,13 +18,11 @@ class Interactive_Html_Generator:
         build_nr = self.build_nr_now("build_nr")
 
 
-
         # --- TRAINING --- |
         file_path = main_path + "/training"
         html_content += "<div id='content_training' class='display'><h2>TRAINING</h2>"
 
         # --- Accuracy & Loss --- |
-        
         full_file_path = os.path.join(os.getcwd(), f"{file_path}/val_acc_and_loss{build_nr}.json")
         html_content += "<h3>" + f"{file_path}/val_acc_and_loss{build_nr}.json" + "<h3>"
 
@@ -42,10 +38,8 @@ class Interactive_Html_Generator:
 
             html_content += self._html_data.html_accuracy_loss_chart(data_al_x, data_al_accuracy, data_al_val_accuracy, data_al_loss, data_al_val_loss)
 
-
         # --- |
         html_content += "</div>"
-
 
 
         # --- EVALUATION --- |
@@ -53,7 +47,6 @@ class Interactive_Html_Generator:
         html_content += "<div id='content_evaluate' class='display'><h2>EVALUATION</h2>"
 
         # --- Confusion Matrix --- |
-
         full_file_path = os.path.join(os.getcwd(), f"{file_path}/confusion_matrix{build_nr}.json")
         html_content += "<h3>" + f"{file_path}/confusion_matrix{build_nr}.json" + "<h3>"
         if os.path.exists(full_file_path):
@@ -66,25 +59,13 @@ class Interactive_Html_Generator:
 
             html_content += self._html_data.html_heatmap_chart(data_heatmap, heatmap_columns, heatmap_rows, heatmap_max_value)
 
-
         # --- Classification Report --- |
-
         full_file_path = os.path.join(os.getcwd(), f"{file_path}/plot_classification_report{build_nr}.html")
+        html_content += self.create_div_file_html(full_file_path)
         html_content += "<h3>" + f"{file_path}/plot_classification_report{build_nr}.html" + "<h3>"
 
-        if os.path.exists(full_file_path):
-            with open(full_file_path, "r") as file:
-                data_content = file.readlines()
-
-            content_start = data_content.index("<body>\n") + 1
-            content_end = data_content.index("</body>\n")
-            content_content = data_content[content_start:content_end]
-            content_string = "".join(content_content)
-            html_content += content_string
-
-
+        # --- |
         html_content += "</div>"
-
 
 
         # --- ANALYZE --- |
@@ -93,50 +74,19 @@ class Interactive_Html_Generator:
 
         # --- PCS & Mean Softmax Score --- |
         full_file_path = os.path.join(os.getcwd(), f"{file_path}/plot_pcs_mean_softmax{build_nr}.html")
+        html_content += self.create_div_file_html(full_file_path)
         html_content += "<h3>" + f"{file_path}/plot_pcs_mean_softmax{build_nr}.html" + "<h3>"
 
-        if os.path.exists(full_file_path):
-            with open(full_file_path, "r") as file:
-                data_content = file.readlines()
-
-            content_start = data_content.index("<body>\n") + 1
-            content_end = data_content.index("</body>\n")
-            content_content = data_content[content_start:content_end]
-            content_string = "".join(content_content)
-            html_content += content_string
-
-
         # --- Entropy Scores --- |
-
         full_file_path = os.path.join(os.getcwd(), f"{file_path}/plot_dist_entropy_scores{build_nr}.html")
+        html_content += self.create_div_file_html(full_file_path)
         html_content += "<h3>" + f"{file_path}/plot_dist_entropy_scores{build_nr}.html" + "<h3>"
-
-        if os.path.exists(full_file_path):
-            with open(full_file_path, "r") as file:
-                data_content = file.readlines()
-
-            content_start = data_content.index("<body>\n") + 1
-            content_end = data_content.index("</body>\n")
-            content_content = data_content[content_start:content_end]
-            content_string = "".join(content_content)
-            html_content += content_string
-
 
         # --- Entropy Scatter Scores --- |
 
         full_file_path = os.path.join(os.getcwd(), f"{file_path}/plot_predictive_conf_entropy_scores{build_nr}.html")
+        html_content += self.create_div_file_html(full_file_path)
         html_content += "<h3>" + f"{file_path}/plot_predictive_conf_entropy_scores{build_nr}.html" + "<h3>"
-
-        if os.path.exists(full_file_path):
-            with open(full_file_path, "r") as file:
-                data_content = file.readlines()
-
-            content_start = data_content.index("<body>\n") + 1
-            content_end = data_content.index("</body>\n")
-            content_content = data_content[content_start:content_end]
-            content_string = "".join(content_content)
-            html_content += content_string
-
         
         # --- |
         html_content += "</div>"
@@ -166,10 +116,8 @@ class Interactive_Html_Generator:
 
         # need to add try and catch for to find if the file exist
 
-
-
         # --- ALL AI MODEL --- |
-        file_path = main_path + "/?????????????????"
+  
         html_content += "<div id='content_analyze' class='display'><h2>ALL AI MODEL</h2>"
 
         # Show pie chart of all ai model accurcy, so they know what to chooce
@@ -184,7 +132,6 @@ class Interactive_Html_Generator:
 
         self.build_list_info("build_nr")
     
-
 
     def create_cheack_file(self):
         default_data = {
@@ -201,6 +148,8 @@ class Interactive_Html_Generator:
         if not os.path.exists(file_path):
             with open(file_path, 'w') as file:
                 json.dump(default_data, file, indent=4)
+
+
 
 
     def build_nr_now(self, option):
@@ -231,3 +180,16 @@ class Interactive_Html_Generator:
         with open(full_file_path, "w") as file:
             json.dump(data_build_info, file, indent=4)
 
+
+    def create_div_file_html(self, full_file_path):
+        html_content = ""
+        if os.path.exists(full_file_path):
+            with open(full_file_path, "r") as file:
+                data_content = file.readlines()
+
+            content_start = data_content.index("<body>\n") + 1
+            content_end = data_content.index("</body>\n")
+            content_content = data_content[content_start:content_end]
+            content_string = "".join(content_content)
+            html_content += content_string
+        return html_content
