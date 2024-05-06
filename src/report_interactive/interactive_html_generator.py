f376421c7e19e7aa8155f38b2c91cbbd7fb85e44
Harykaran Lambotharan <l.harykaran@gmail.com>
2024-05-06 16:09:26 +0200

    updated

--- src/report_interactive/interactive_html_generator.py
+++ src/report_interactive/interactive_html_generator.py
@@ -70,7 +70,7 @@ class Interactive_Html_Generator:
         # --- plot_predictions --- |
         full_file_path = os.path.join(os.getcwd(), f"{file_path}/plot_predictions{build_nr}.png")
         html_content += f"<img src={full_file_path} alt='plot predictions'>"
-        html_content += "<p>" + f"{file_path}/plot_classification_report{build_nr}.html" + "<p>"
+        html_content += "<p>" + f"{file_path}/plot_predictions{build_nr}.html" + "<p>"
 
         # --- |
         html_content += "</div>"
