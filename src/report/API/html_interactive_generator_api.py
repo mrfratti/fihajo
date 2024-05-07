4624d35d97d39db6777f90b2e1bb458b2face9b8
Harykaran Lambotharan <l.harykaran@gmail.com>
2024-05-07 02:34:12 +0200

    updated

--- src/report/API/html_interactive_generator_api.py
+++ src/report/API/html_interactive_generator_api.py
@@ -3,39 +3,38 @@ from src.report.interactive.html_generator import InteractiveHtmlGenerator
 from src.report.image_data import ImageData
 
 
+# class InteractiveHtmlGeneratorApi:
+#     """API for HtmlGenerator"""
+
+#     def __init__(self, report_location, report_filename, images):
+#         self._report = HtmlData()
+#         self._report.html_store_location = report_location
+#         self._report.filename = report_filename
+#         self._generator = InteractiveHtmlGenerator()
+#         self._generator.html_report = self._report
+#         for image in images:
+#             image_data = ImageData()
+#             image_data.header_image = image["image_header"]
+#             image_data.image_location = image["image_location"]
+#             image_data.about_image = image["about_image"]
+#             self._generator.image_data = image_data
+#         self._generator.write_html()
+
+
+
+
 class InteractiveHtmlGeneratorApi:
-    """API for HtmlGenerator"""
 
     def __init__(self, report_location, report_filename, images):
         self._report = HtmlData()
-        self._report.html_store_location = report_location
-        self._report.filename = report_filename
         self._generator = InteractiveHtmlGenerator()
         self._generator.html_report = self._report
-        for image in images:
-            image_data = ImageData()
-            image_data.header_image = image["image_header"]
-            image_data.image_location = image["image_location"]
-            image_data.about_image = image["about_image"]
-            self._generator.image_data = image_data
+        self._report.head = f"<div>{report_location}</div>" + f"<div>{report_filename}</div>"
+        self._report.main = "<div>Zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz</div>"
+        self._report.html_store_location="./"
+        self._report.filename = "test_test_index"
+
         self._generator.write_html()
-
-
-
-
-# class InteractiveHtmlGeneratorApi:
-
-#     # def __init__(self, report_location, report_filename, images):
-#     def __init__(self):
-#         self._report = HtmlData()
-#         self._generator = HtmlGenerator()
-#         self._generator.html_report = self._report
-#         self._report.head = "<div>SUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUp</div>"
-#         self._report.main = "<div>Zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz</div>"
-#         self._report.html_store_location="./"
-#         self._report.filename = "test_index"
-
-#         self._generator.write_html()
 
 # if __name__ == "__main__":
 #     InteractiveHtmlGeneratorApi()
