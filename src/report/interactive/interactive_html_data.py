fbc6448958878ee1f22b11c1da8d3238d00f0c6e
Harykaran Lambotharan <l.harykaran@gmail.com>
2024-05-11 13:37:29 +0200

    under testing

--- src/report/interactive/interactive_html_data.py
+++ src/report/interactive/interactive_html_data.py
@@ -1,6 +1,195 @@
 import json
 
 
+def html_accuracy_loss_chart(data_accuracy_loss, title):
+    
+    data_al_x = data_accuracy_loss["x"]
+    data_al_accuracy = data_accuracy_loss["accuracy"]
+    data_al_val_accuracy = data_accuracy_loss["val_accuracy"]
+    data_al_loss = data_accuracy_loss["loss"]
+    data_al_val_loss = data_accuracy_loss["val_loss"]
+
+    html_content = f"""
+
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Training Accuracy & Loss</title>
+            <title>{title}</title>
+            <script src="https://cdn.jsdelivr.net/npm/echarts@5.3.2/dist/echarts.min.js"></script>
+            <style>
+                body {{
+                    background-color: #f2f2d2;
+                }}
+                .charts_box {{
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    margin: 50px auto;
+                }}
+                .chart_line_1 {{
+                    width: 600px;
+                    height: 400px;
+                }}
+            </style>
+        </head>
+        <body>
+            
+            <h2>Training</h2>
+            <div class="charts_box">
+                <div id="chart_accuracy" class="chart_line_1"></div>
+                <div id="chart_loss" class="chart_line_1"></div>
+            </div>
+
+            <script>
+                
+                var js_chart_accuracy = echarts.init(document.getElementById("chart_accuracy"));
+                var js_chart_loss = echarts.init(document.getElementById("chart_loss"));
+
+                var option_accuracy = {{
+                    title: {{
+                        text: "Accuracy"
+                    }},
+                    legend: {{
+                        data: ["Accuracy", "Validation Accuracy"]
+                    }},
+
+                    tooltip: {{
+                        trigger: "axis",
+                        axisPointer: {{
+                            type: "cross"
+                        }}
+                    }},
+
+                    toolbox: {{
+                        feature: {{
+                            dataZoom: {{
+                                yAxisIndex: "none"
+                            }},
+                            restore: {{}},
+                            magicType: {{ type: ["line", "bar"] }}
+                        }}
+                    }},
+                    grid: {{
+                        left: "5%",
+                        right: "5%",
+                        bottom: "5%",
+                        containLabel: true
+                    }},
+                    
+                    xAxis: {{
+                        type: "category",
+                        boundaryGap: false,
+                        data: {json.dumps(data_al_x)}
+                    }},
+                    yAxis: {{
+                        type: "value"
+                    }},
+
+                    series: [
+                        {{
+                            name: "Accuracy",
+                            type: "line",
+                            data: {json.dumps(data_al_accuracy)},
+                            smooth: true,
+                            markPoint: {{
+                                label: {{
+                                    formatter: function (param) {{
+                                        return param.name + ": " + param.value.toFixed(2);
+                                    }}
+                                }},
+                                data: [
+                                    {{ type: "max", name: "" }},
+                                    {{ type: "min", name: "" }}
+                                ]
+                            }}
+                        }},
+                        {{
+                            name: "Validation Accuracy",
+                            type: "line",
+                            data: {json.dumps(data_al_val_accuracy)},
+                            smooth: true
+                        }}
+                    ]
+                }};
+
+                var option_loss = {{
+                    title: {{
+                        text: "Loss"
+                    }},
+                    legend: {{
+                        data: ["Loss", "Validation Loss"]
+                    }},
+
+                    tooltip: {{
+                        trigger: "axis",
+                        axisPointer: {{
+                            type: "cross"
+                        }}
+                    }},
+                    toolbox: {{
+                        feature: {{
+                            dataZoom: {{
+                                yAxisIndex: "none"
+                            }},
+                            restore: {{}},
+                            magicType: {{ type: ["line", "bar"] }}
+                        }}
+                    }},
+                    grid: {{
+                        left: "5%",
+                        right: "5%",
+                        bottom: "5%",
+                        containLabel: true
+                    }},
+
+                    xAxis: {{
+                        type: "category",
+                        boundaryGap: false,
+                        data: {json.dumps(data_al_x)}
+                    }},
+                    yAxis: {{
+                        type: "value"
+                    }},
+
+                    series: [
+                        {{
+                            name: "Loss",
+                            type: "line",
+                            data: {json.dumps(data_al_loss)},
+                            smooth: true,
+                            markPoint: {{
+                                label: {{
+                                    formatter: function (param) {{
+                                        return param.name + ": " + param.value.toFixed(2);
+                                    }}
+                                }},
+                                data: [
+                                    {{ type: "max", name: "" }},
+                                    {{ type: "min", name: "" }}
+                                ]
+                            }}
+                        }},
+                        {{
+                            name: "Validation Loss",
+                            type: "line",
+                            data: {json.dumps(data_al_val_loss)},
+                            smooth: true
+                        }}
+                    ]
+                }};
+
+                js_chart_accuracy.setOption(option_accuracy);
+                js_chart_loss.setOption(option_loss);
+
+            </script>
+        </body>
+    </html>
+    """
+    return html_content
+
+
+
 def html_heatmap_chart(data_heatmap):
 
     heatmap_columns = list({each_data["column"] for each_data in data_heatmap})
@@ -100,6 +289,3 @@ def html_heatmap_chart(data_heatmap):
     """
 
     return html_content
-
-
-
